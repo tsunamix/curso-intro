4f0822efc8566ec43511bef68a17bd5150f14ed3
Dani <d.garcia@barcelo.com>
2022-08-24 16:56:47 +0200

    Updates

---
+++ precache-manifest.d8541baa88c4360a099e9e8965b0373d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f445c938c0bb23d54c34",
+    "url": "/curso-intro/static/css/main.86485a8a.chunk.css"
+  },
+  {
+    "revision": "f445c938c0bb23d54c34",
+    "url": "/curso-intro/static/js/main.f445c938.chunk.js"
+  },
+  {
+    "revision": "bee36d96ecf68392c5e3",
+    "url": "/curso-intro/static/js/1.bee36d96.chunk.js"
+  },
+  {
+    "revision": "4cf127d1e40941ebb39c",
+    "url": "/curso-intro/static/js/runtime~main.4cf127d1.js"
+  },
+  {
+    "revision": "a921b8b8f8531a0b29f3a76dde2eb200",
+    "url": "/curso-intro/static/media/check.a921b8b8.svg"
+  },
+  {
+    "revision": "abb8a771e188ec9fd23ba3ef323d6f24",
+    "url": "/curso-intro/static/media/delete.abb8a771.svg"
+  },
+  {
+    "revision": "f58582aa46453344d176a4a8a397a722",
+    "url": "/curso-intro/index.html"
+  }
+];
